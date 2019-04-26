d047d48eb5d07ecb457b649eef1a0be1a9f21fa5
daoudakala <kalada15@gmail.com>
2019-04-26 23:18:53 +0000

    first commit

---
+++ effets.js
@@ -0,0 +1,15 @@
+// function(e)
+// {
+//     e.PreventDefault();
+// }
+
+// $(document).ready(function()
+// {
+//     // $("#titre").hide(4000);
+//     // $("#titre").show(4000);
+//     // $("#titre").slideUp(4000);
+//     // $("#titre").slideDown(4000);
+// //     $("#myModal").modal() 
+// }
+
+// )
